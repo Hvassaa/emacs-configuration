5f18260d78984f51d3502334a2f629ee5334e86e
Rasmus T. Bjerg <rs.bjerg@gmail.com>
2026-07-14 23:16:31 +0200

    reboot

--- init.el
+++ init.el
@@ -1,47 +1,12 @@
-(setq custom-file "~/.emacs.d/custom.el")
-(require 'package)
-(add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/") t)
-(package-initialize)
-
-(use-package which-key
-  :ensure t
-  :config (which-key-mode t))
-
-(use-package vertico
-  :ensure t
-  :config (vertico-mode t))
-
-(use-package marginalia
-  :ensure t
-  :init (marginalia-mode))
-
-(use-package orderless
-  :ensure t
-  :custom
-  (completion-styles '(orderless basic))
-  (completion-category-overrides '((file (styles basic partial-completion)))))
-
-(use-package lsp-mode
-  :ensure t
-  :init (setq lsp-keymap-prefix "C-c l")
-  :hook ((lsp-mode . lsp-enable-which-key-integration))
-  :commands lsp)
-(use-package lsp-java
-  :config (add-hook 'java-mode-hook 'lsp)
-  :ensure t)
-(use-package lsp-ui
-  :ensure t)
-(use-package company
-  :ensure t
-  :custom
-  (company-minimum-prefix-length 1)
-  (company-idle-delay 0.1))
-
-(use-package flycheck
-  :ensure t)
-(setq gc-cons-threshold 100000000)
-(setq read-process-output-max (* 1024 1024))
-
-
-(global-display-line-numbers-mode t)
 (setq ring-bell-function 'ignore)
+(fido-vertical-mode 1)
+(which-key-mode 1)
+(global-display-line-numbers-mode 1)
+(pixel-scroll-mode 1)
+
+(use-package corfu
+  :ensure t
+  :custom ((global-corfu-mode 1)
+	   (corfu-auto 't)
+	   (corfu-auto-prefix 1)))
+

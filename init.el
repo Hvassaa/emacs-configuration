c6228c78c6801b52c11f10549f59f77566e745b3
Rasmus T. Bjerg <rs.bjerg@gmail.com>
2026-07-30 23:50:11 +0200

    gdscript, more settings

--- init.el
+++ init.el
@@ -1,12 +1,24 @@
-(setq ring-bell-function 'ignore)
-(fido-vertical-mode 1)
+(setopt custom-file "~/.emacs.d/custom.el")
+(setopt ring-bell-function 'ignore)
+(setopt truncate-lines t)
+(setopt make-backup-files nil)
+(setopt inhibit-startup-message t)
 (which-key-mode 1)
 (global-display-line-numbers-mode 1)
-(pixel-scroll-mode 1)
+(pixel-scroll-precision-mode 1)
+(load-theme 'modus-vivendi)
 
 (use-package corfu
   :ensure t
   :custom ((global-corfu-mode 1)
 	   (corfu-auto 't)
-	   (corfu-auto-prefix 1)))
+	   (corfu-auto-prefix 1)
+	   (corfu-preview-current nil)
+	   (corfu-popupinfo-mode 1)
+	   (corfu-popupinfo-delay 0.1)))
 
+(use-package gdscript-mode
+  :vc (:url "git@github.com:godotengine/emacs-gdscript-mode.git"))
+
+(use-package sly
+  :ensure t)
